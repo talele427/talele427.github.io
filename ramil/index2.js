fcf0088e883065e049908182f0e582141d221d9a
talele427 <soham.talele@gmail.com>
2022-03-31 13:51:58 +0530

    added audio playback ramil

--- ramil/index2.js
+++ ramil/index2.js
@@ -23,3 +23,36 @@ let ramil = document.getElementById("background");
 let title = document.getElementById("title");
 let social = document.getElementById("socials");
 let socials = document.querySelectorAll(".social");
+
+let tracklist=["bhoola", "gulmohar", "tu", "daastaan", "dhundho"]
+
+$('.music > div > div > img').click(function(){
+    let a = this.src.split('/');
+    let b = a[a.length-1].split('.');
+    if(b[0]!="phir"){
+        pauseAllMusic();
+        document.getElementById(b[0]).play();
+        $('.music > p').html("click here for the full discography")
+        if(this.classList.includes("active")){
+            document.getElementById(b[0]).pause();
+            this.classList.remove('active')
+        }else{
+            this.classList.add('active')
+        }
+    }
+    
+})
+
+function pauseAllMusic(){
+    for (let track of tracklist){
+        document.getElementById(track).pause();
+    }
+    let icons = document.querySelectorAll('.music > div > div > img');
+    for (let icon of icons){
+        icon.classList.remove('active');
+    }
+}
+
+setInterval(()=>{
+    console.log(title)
+},2000)
